694798adceb402a6d6bae3424d518e0a92354047
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-12-03 10:37:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let currentCircle = 1;
+    const totalCircles = 5;
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+
+    function changeCircle(direction) {
+        const newCircle = currentCircle + direction;
+
+        if (newCircle >= 1 && newCircle <= totalCircles) {
+            document.getElementById(`circle-${currentCircle}`).classList.remove('active');
+            document.getElementById(`circle-${newCircle}`).classList.add('active');
+            currentCircle = newCircle;
+
+            // Update button states
+            prevButton.disabled = currentCircle === 1;
+            nextButton.disabled = currentCircle === totalCircles;
+        }
+    }
